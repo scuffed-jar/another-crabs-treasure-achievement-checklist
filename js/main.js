9db1ee17f7252c8585e28a1b74d05f40a43d0be0
Intahjer <intahjer@gmail.com>
2025-01-03 19:31:05 -0800

    Updated with the last checklist.

--- js/main.js
+++ js/main.js
@@ -1,138 +1,44 @@
-(function($) {
+var profilesKey = 'Default Key';
 
-    var defaultProfiles = {
-        'current': 'Default Profile'
-    };
-    defaultProfiles[profilesKey] = {
-        'Default Profile': {
-            checklistData: {}
-        }
-    }
-    var profiles = $.jStorage.get(profilesKey, defaultProfiles);
+(function($) {
+    'use strict';
+
+    var profiles = $.jStorage.get(profilesKey, {});
+
+    if (!('current' in profiles)) profiles.current = 'Default Profile';
+    if (!(profilesKey in profiles)) profiles[profilesKey] = {};
+    initializeProfile(profiles.current);
 
     jQuery(document).ready(function($) {
 
-        // TODO Find a better way to do this in one pass
-        $('ul li li').each(function(index) {
-            if ($(this).attr('data-id')) {
-                addCheckbox(this);
-            }
-        });
-        $('ul li').each(function(index) {
-            if ($(this).attr('data-id')) {
-                addCheckbox(this);
-            }
+        $('ul li[data-id]').each(function() {
+            addCheckbox(this);
         });
 
-        populateProfiles();
+        $("a[href^='http']").attr('target','_blank');
 
-        $('input[type="checkbox"]').click(function() {
+        $('.checkbox input[type="checkbox"]').click(function() {
             var id = $(this).attr('id');
             var isChecked = profiles[profilesKey][profiles.current].checklistData[id] = $(this).prop('checked');
-            $(this).parent().parent().find('li > label > input[type="checkbox"]').each(function() {
-                var id = $(this).attr('id');
-                profiles[profilesKey][profiles.current].checklistData[id] = isChecked;
-                $(this).prop('checked', isChecked);
-            });
+            if (isChecked === true) {
+              $('[data-id="'+id+'"] label').addClass('completed');
+            } else {
+              $('[data-id="'+id+'"] label').removeClass('completed');
+            }
             $.jStorage.set(profilesKey, profiles);
             calculateTotals();
-        });
-
-        $('#profiles').change(function(event) {
-            profiles.current = $(this).val();
-            $.jStorage.set(profilesKey, profiles);
-            populateChecklists();
-        });
-
-        $('#profileAdd').click(function() {
-            $('#profileModalTitle').html('Add Profile');
-            $('#profileModalName').val('');
-            $('#profileModalAdd').show();
-            $('#profileModalUpdate').hide();
-            $('#profileModalDelete').hide();
-            $('#profileModal').modal('show');
-        });
-
-        $('#profileEdit').click(function() {
-            $('#profileModalTitle').html('Edit Profile');
-            $('#profileModalName').val(profiles.current);
-            $('#profileModalAdd').hide();
-            $('#profileModalUpdate').show();
-            if (canDelete()) {
-                $('#profileModalDelete').show();
-            } else {
-                $('#profileModalDelete').hide();
-            }
-            $('#profileModal').modal('show');
-        });
-
-        $('#profileModalAdd').click(function(event) {
-            event.preventDefault();
-            var profile = $.trim($('#profileModalName').val());
-            if (profile.length > 0) {
-                if (typeof profiles[profilesKey][profile] == 'undefined') {
-                    profiles[profilesKey][profile] = { checklistData: {} };
-                }
-                profiles.current = profile;
-                $.jStorage.set(profilesKey, profiles);
-                populateProfiles();
-                populateChecklists();
-            }
-            $('#profileModal').modal('hide');
-        });
-
-        $('#profileModalUpdate').click(function(event) {
-            event.preventDefault();
-            var newName = $.trim($('#profileModalName').val());
-            if (newName.length > 0 && newName != profiles.current) {
-                profiles[profilesKey][newName] = profiles[profilesKey][profiles.current];
-                delete profiles[profilesKey][profiles.current];
-                profiles.current = newName;
-                $.jStorage.set(profilesKey, profiles);
-                populateProfiles();
-            }
-            $('#profileModal').modal('hide');
-        });
-
-        $('#profileModalDelete').click(function(event) {
-            event.preventDefault();
-            if (!canDelete()) {
-                return;
-            }
-            if (!confirm('Are you sure?')) {
-                return;
-            }
-            delete profiles[profilesKey][profiles.current];
-            profiles.current = getFirstProfile();
-            $.jStorage.set(profilesKey, profiles);
-            populateProfiles();
-            populateChecklists();
-            $('#profileModal').modal('hide');
-        });
-
-        $('#profileModalClose').click(function(event) {
-            event.preventDefault();
-            $('#profileModal').modal('hide');
         });
 
         calculateTotals();
 
     });
 
-    function populateProfiles() {
-        $('#profiles').empty();
-        $.each(profiles[profilesKey], function(index, value) {
-            $('#profiles').append($("<option></option>").attr('value', index).text(index));
-        });
-        $('#profiles').val(profiles.current);
-    }
-
-    function populateChecklists() {
-        $('input[type="checkbox"]').prop('checked', false);
-        $.each(profiles[profilesKey][profiles.current].checklistData, function(index, value) {
-            $('#' + index).prop('checked', value);
-        });
-        calculateTotals();
+    function initializeProfile(profile_name) {
+        if (!(profile_name in profiles[profilesKey])) profiles[profilesKey][profile_name] = {};
+        if (!('checklistData' in profiles[profilesKey][profile_name]))
+            profiles[profilesKey][profile_name].checklistData = {};
+        if (!('collapsed' in profiles[profilesKey][profile_name]))
+            profiles[profilesKey][profile_name].collapsed = {};
     }
 
     function calculateTotals() {
@@ -145,7 +51,7 @@
                 var count = 0, checked = 0;
                 for (var j = 1; ; j++) {
                     var checkbox = $('#' + type + '_' + i + '_' + j);
-                    if (checkbox.length == 0) {
+                    if (checkbox.length === 0) {
                         break;
                     }
                     count++;
@@ -155,47 +61,51 @@
                         overallChecked++;
                     }
                 }
-                if (checked == count) {
-                    this.innerHTML = $('#' + type + '_nav_totals_' + i)[0].innerHTML = '[DONE]';
+                if (checked === count) {
+                    this.innerHTML = $('#' + type + '_nav_totals_' + i)[0].innerHTML = 'DONE';
                     $(this).removeClass('in_progress').addClass('done');
+                    $(this).parent('h3').addClass('completed');// Hide heading for completed category
                     $($('#' + type + '_nav_totals_' + i)[0]).removeClass('in_progress').addClass('done');
                 } else {
-                    this.innerHTML = $('#' + type + '_nav_totals_' + i)[0].innerHTML = '[' + checked + '/' + count + ']';
+                    this.innerHTML = $('#' + type + '_nav_totals_' + i)[0].innerHTML =  checked + '/' + count;
                     $(this).removeClass('done').addClass('in_progress');
+                    $(this).parent('h3').removeClass('completed');// Show heading for not yet completed category
                     $($('#' + type + '_nav_totals_' + i)[0]).removeClass('done').addClass('in_progress');
                 }
+                $(this).parent('h3').next('div').children('h4').addClass('completed');// Hide all subheadings...
+                $(this).parent('h3').next('div').children('ul').children('li').children('div').children('label:not(.completed)').parent('div').parent('li').parent('ul').prev('h4').removeClass('completed');// ... except those where not all entries below the subheading are labeled as completed
             });
-            if (overallChecked == overallCount) {
-                this.innerHTML = '[DONE]';
+            if (overallChecked === overallCount) {
+                this.innerHTML = 'DONE';
                 $(this).removeClass('in_progress').addClass('done');
             } else {
-                this.innerHTML = '[' + overallChecked + '/' + overallCount + ']';
+                this.innerHTML = overallChecked + '/' + overallCount;
                 $(this).removeClass('done').addClass('in_progress');
             }
+        document.getElementById("profileText").value = JSON.stringify(profiles);
         });
     }
 
     function addCheckbox(el) {
-        var lines = $(el).html().split('\n');
-        lines[0] = '<label class="checkbox"><input type="checkbox" id="' + $(el).attr('data-id') + '">' + lines[0] + '</label>';
-        $(el).html(lines.join('\n'));
-        if (profiles[profilesKey][profiles.current].checklistData[$(el).attr('data-id')] == true) {
-            $('#' + $(el).attr('data-id')).prop('checked', true);
-        }
-    }
+        var $el = $(el);
+        var content = $el.html().split('\n')[0];
+        var sublists = $el.children('ul');
 
-    function canDelete() {
-        var count = 0;
-        $.each(profiles[profilesKey], function(index, value) {
-            count++;
-        });
-        return (count > 1);
-    }
+        content =
+            '<div class="checkbox">' +
+                '<label>' +
+                    '<input type="checkbox" id="' + $el.attr('data-id') + '">' +
+                    '<span class="item_content">' + content + '</span>' +
+                '</label>' +
+            '</div>';
 
-    function getFirstProfile() {
-        for (var profile in profiles[profilesKey]) {
-            return profile;
+        $el.html(content).append(sublists);
+
+        if (profiles[profilesKey][profiles.current].checklistData[$el.attr('data-id')] === true) {
+            $('#' + $el.attr('data-id')).prop('checked', true);
+            $('label', $el).addClass('completed');
         }
     }
 
 })( jQuery );
+
